bce89d4d180ba9d40aace3b9519099e7cbb773a2
AtharvaPatange <164712914+AtharvaPatange@users.noreply.github.com>
2024-12-22 18:51:23 +0530

    CORS

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://ecommerce-frontend-murex-six.vercel.app/'], // Frontend URLs
+  origin: ['https://ecommerce-frontend-murex-six.vercel.app/', 'http://localhost:3000', 'http://localhost:5000'], // Frontend URLs
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
